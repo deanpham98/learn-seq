8c942b74e4f6b515f23cd84d2a326af71580881c
nghia-dlbox <nghia.vq96@gmail.com>
2020-10-13 18:49:32 +0800

    update config

--- exp/filter-force-1i-12/config.py
+++ exp/filter-force-1i-12/config.py
@@ -208,8 +208,8 @@ env_config = {
     "primitive_list": primitive_list,
     "peg_pos_range": ([-0.2]*3, [0.2]*3),
     "peg_rot_range": ([-1]*3, [1]*3),
-    "initial_pos_range": ([-0.002]*2+ [-0.001], [0.002]*2+ [0.001]),
-    "initial_rot_range": ([-2*np.pi/180]*3, [2*np.pi/180]*3),
+    "initial_pos_range": ([-0.001]*2+ [-0.001], [0.001]*2+ [0.001]),
+    "initial_rot_range": ([-1*np.pi/180]*3, [1*np.pi/180]*3),
     "goal_thresh": GOAL_THRESH,
     "wrapper": StructuredActionSpaceWrapper,
     "wrapper_kwargs": {
