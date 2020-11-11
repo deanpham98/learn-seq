aed919630551faea2b323bfcc76e7a5d1ca9a5b7
nvuong21 <nghia.vq96@gmail.com>
2020-11-11 17:28:57 +0800

    update example
    Signed-off-by: nvuong21 <nghia.vq96@gmail.com>

--- exp/example_rlpyt/config.py
+++ exp/example_rlpyt/config.py
@@ -9,35 +9,37 @@ from learn_seq.controller.hybrid import StateRecordHybridController
 
 #----- primitive params
 NO_QUANTIZATION = 2
-SPEED_FACTOR_RANGE = [0.01, 0.05]
-FORCE_THRESH_RANGE = [5, 10]
-TORQUE_THRESH_RANGE = [0.2, 1]
+SPEED_FACTOR_RANGE = [0.01, 0.02]
+SLIDING_SPEED_FACTOR_RANGE = [0.008, 0.015]
+FORCE_THRESH_RANGE = [8, 15]
+TORQUE_THRESH_RANGE = [0.1, 0.5]
 TRANSLATION_DISPLACEMENT_RANGE = [0.001, 0.005]
 ROTATION_DISPLACEMENT_RANGE = [np.pi/180, 5*np.pi/180]
-INSERTION_FORCE_RANGE = [10., 25]
+INSERTION_FORCE_RANGE = [5., 12]
 KD_ADMITTANCE_ROT_RANGE = [0.01, 0.15]
+ROTATION_TO_TRANSLATION_FACTOR = 8
 SAFETY_FORCE = 15.
-SAFETY_TORQUE = 2.
+SAFETY_TORQUE = 1.
 # controller gains
-KP_DEFAULT = [1000.]*3 + [60.]*3
+KP_DEFAULT = [2500.] + [1500]*2 + [60.]*2 + [30.]
 KD_DEFAULT = [2*np.sqrt(i) for i in KP_DEFAULT]
 TIMEOUT = 2.
 
 # TODO change in environment
 HOLE_DEPTH = 0.02
-DEPTH_THRESH = 0.95 # the goal is achieved when insertion depth > HOLE_DEPTH * DEPTH_THRESH
+GOAL_THRESH = 2e-3
 TRAINING_STEP = 1000000
 SEED = 18
 
 # ----- Primitive config
 primitive_list = []
 # move down until contact (16 primitives)
-for i in range(4):
-    for j in range(4):
-        dv = (SPEED_FACTOR_RANGE[1] - SPEED_FACTOR_RANGE[0])/4
+for i in range(NO_QUANTIZATION):
+    for j in range(NO_QUANTIZATION):
+        dv = (SPEED_FACTOR_RANGE[1] - SPEED_FACTOR_RANGE[0])/NO_QUANTIZATION
         v = SPEED_FACTOR_RANGE[0] + dv/2 + i*dv
 
-        dfs = (FORCE_THRESH_RANGE[1] - FORCE_THRESH_RANGE[0]) / 4
+        dfs = (FORCE_THRESH_RANGE[1] - FORCE_THRESH_RANGE[0]) / NO_QUANTIZATION
         fs = FORCE_THRESH_RANGE[0] + dfs/2 + j*dv
 
         param = dict(u=np.array([0, 0, -1, 0, 0, 0]),
@@ -49,7 +51,7 @@ for i in range(4):
         primitive_list.append(("move2contact", param))
 
 # displacement free space
-vd = 0.05
+vd = 0.01
 for i in range(3):  # x, y, z translation
     move_dir = np.zeros(6)
     for j in range(NO_QUANTIZATION):
@@ -76,7 +78,7 @@ for i in range(3):  # x, y, z rotation
         p = ROTATION_DISPLACEMENT_RANGE[0] + dp/2 + j*dp
         move_dir[i+3] = 1
         param = dict(u=move_dir,
-                     s=v, fs=fs,
+                     s=vd*ROTATION_TO_TRANSLATION_FACTOR, fs=SAFETY_TORQUE,
                      ft=np.zeros(6),
                      delta_d=p,
                      kp=KP_DEFAULT,
@@ -96,8 +98,8 @@ for i in range(2):  # x, y translation
     move_dir = np.zeros(6)
     for j in range(NO_QUANTIZATION):
         for k in range(NO_QUANTIZATION):
-            dv = (SPEED_FACTOR_RANGE[1] - SPEED_FACTOR_RANGE[0])/NO_QUANTIZATION
-            v = SPEED_FACTOR_RANGE[0] + dv/2 + j*dv
+            dv = (SLIDING_SPEED_FACTOR_RANGE[1] - SLIDING_SPEED_FACTOR_RANGE[0])/NO_QUANTIZATION
+            v = SLIDING_SPEED_FACTOR_RANGE[0] + dv/2 + j*dv
 
             dfs = (FORCE_THRESH_RANGE[1] - FORCE_THRESH_RANGE[0]) / NO_QUANTIZATION
             fs = FORCE_THRESH_RANGE[0] + dfs/2 + k*dv
@@ -117,15 +119,15 @@ for i in range(3):  # x, y, z rotation
     move_dir = np.zeros(6)
     for j in range(NO_QUANTIZATION):
         for k in range(NO_QUANTIZATION):
-            dv = (SPEED_FACTOR_RANGE[1] - SPEED_FACTOR_RANGE[0])/NO_QUANTIZATION
-            v = SPEED_FACTOR_RANGE[0] + dv/2 + j*dv
+            dv = (SLIDING_SPEED_FACTOR_RANGE[1] - SLIDING_SPEED_FACTOR_RANGE[0])/NO_QUANTIZATION
+            v = SLIDING_SPEED_FACTOR_RANGE[0] + dv/2 + j*dv
 
-            dfs = (FORCE_THRESH_RANGE[1] - FORCE_THRESH_RANGE[0]) / NO_QUANTIZATION
-            fs = FORCE_THRESH_RANGE[0] + dfs/2 + k*dv
+            dfs = (TORQUE_THRESH_RANGE[1] - TORQUE_THRESH_RANGE[0]) / NO_QUANTIZATION
+            fs = TORQUE_THRESH_RANGE[0] + dfs/2 + k*dv
 
             move_dir[i+3] = 1
             param = dict(u=move_dir,
-                         s=v, fs=fs,
+                         s=v*ROTATION_TO_TRANSLATION_FACTOR, fs=fs,
                          ft=np.array([0, 0, -3, 0, 0, 0.]),
                          kp=KP_DEFAULT,
                          kd=KD_DEFAULT,
@@ -135,7 +137,7 @@ for i in range(3):  # x, y, z rotation
             primitive_list.append(("move2contact", deepcopy(param)))
 
 # displacement on plane
-vd = 0.02
+vd = 0.01
 for i in range(2):  # x, y tranlation
     move_dir = np.zeros(6)
     for j in range(NO_QUANTIZATION):
@@ -143,7 +145,7 @@ for i in range(2):  # x, y tranlation
         p = TRANSLATION_DISPLACEMENT_RANGE[0] + dp/2 + j*dp
         move_dir[i] = 1
         param = dict(u=move_dir,
-                     s=v, fs=fs,
+                     s=vd, fs=SAFETY_FORCE,
                      ft=np.array([0, 0, -3, 0, 0, 0.]),
                      delta_d=p,
                      kp=KP_DEFAULT,
@@ -161,7 +163,7 @@ for i in range(3):  # x, y, z rotation
         p = ROTATION_DISPLACEMENT_RANGE[0] + dp/2 + j*dp
         move_dir[i+3] = 1
         param = dict(u=move_dir,
-                     s=v, fs=fs,
+                     s=vd*ROTATION_TO_TRANSLATION_FACTOR, fs=SAFETY_TORQUE,
                      ft=np.array([0, 0, -3, 0, 0, 0.]),
                      delta_d=p,
                      kp=KP_DEFAULT,
@@ -175,40 +177,41 @@ for i in range(3):  # x, y, z rotation
 # admittance
 stiffness =[500, 500, 500, 50, 50, 50.]
 damping = [10.]*6
-for j in range(4):
-    for k in range(4):
-        dkd = (KD_ADMITTANCE_ROT_RANGE[1] - KD_ADMITTANCE_ROT_RANGE[0])/4
+for j in range(NO_QUANTIZATION):
+    for k in range(NO_QUANTIZATION):
+        dkd = (KD_ADMITTANCE_ROT_RANGE[1] - KD_ADMITTANCE_ROT_RANGE[0])/NO_QUANTIZATION
         kd = KD_ADMITTANCE_ROT_RANGE[0] + dkd/2 + j*dkd
 
-        df = (INSERTION_FORCE_RANGE[1] - INSERTION_FORCE_RANGE[0]) / 4
+        df = (INSERTION_FORCE_RANGE[1] - INSERTION_FORCE_RANGE[0]) / NO_QUANTIZATION
         f = INSERTION_FORCE_RANGE[0] + df/2 + k*df
 
         param = dict(kd_adt=np.array([0.]*3 + [kd]*3),
                      ft=np.array([0, 0, -f, 0, 0, 0]),
-                     depth_thresh=-HOLE_DEPTH*DEPTH_THRESH,
+                     pt=np.array([0, 0, -HOLE_DEPTH]),
+                     goal_thresh=GOAL_THRESH,
                      kp=stiffness,
                      kd=damping,
                      timeout=TIMEOUT)
         primitive_list.append(("admittance", param))
 
-no_contact_actions = len(primitive_list)
-contact_action_idx = list(range(no_contact_actions))
+#no_contact_actions = len(primitive_list)
+no_contact_actions = len(primitive_list) - no_free_actions
+contact_action_idx = list(range(no_free_actions, no_free_actions + no_contact_actions))
 sub_spaces = [free_action_idx, contact_action_idx]
 
 # ----- train config
 env_config = {
     "id": "learn_seq:MujocoInsertionEnv-v0",
     "primitive_list": primitive_list,
-    "peg_pos_range": ([-0.05]*3, [0.05]*3),
-    "peg_rot_range": ([np.pi - 0.2] + [-0.2]*2, [np.pi + 0.2] + [0.2]*2),
-    "initial_pos_range": ([-0.005]*2+ [-0.002], [0.005]*2+ [0.002]),
-    "initial_rot_range": ([-5*np.pi/180]*3, [5*np.pi/180]*3),
-    "depth_thresh": DEPTH_THRESH,
-    "controller_class": StateRecordHybridController,
+    "peg_pos_range": ([-0.2]*3, [0.2]*3),
+    "peg_rot_range": ([-1]*3, [1]*3),
+    "initial_pos_range": ([-0.002]*2+ [-0.002], [0.002]*2+ [0.002]),
+    "initial_rot_range": ([-2*np.pi/180]*3, [2*np.pi/180]*3),
+    "goal_thresh": GOAL_THRESH,
     "wrapper": StructuredActionSpaceWrapper,
     "wrapper_kwargs": {
-        "hole_pos_error_range": ([-1./1000]*2+ [0.], [1./1000]*2+ [0.]),
-        "hole_rot_error_range": ([-np.pi/180]*3, [np.pi/180]*3),
+        "hole_pos_error_range": ([-1./1000]*2+ [-1./1000], [1./1000]*2+ [1./1000]),
+        "hole_rot_error_range": ([-1*np.pi/180]*3, [1*np.pi/180]*3),
         "spaces_idx_list": sub_spaces
     }
 }
@@ -224,7 +227,7 @@ agent_config = {
 sampler_config = {
     "sampler_class": CpuSampler,
     "sampler_kwargs":{
-        "batch_T": 256, # no samples per iteration
+        "batch_T": 512, # no samples per iteration
         "batch_B": 4, # no environments, this will be divided equally to no. parallel envs
         "max_decorrelation_steps": 10
     }
