b5aa26e52ab3e3d3bbafe32af2a6bf3d983084b4
nvuong21 <nghia.vq96@gmail.com>
2020-11-13 18:11:52 +0800

    fix bug
    Signed-off-by: nvuong21 <nghia.vq96@gmail.com>

--- learn_seq/__init__.py
+++ learn_seq/__init__.py
@@ -5,10 +5,10 @@ register(
     entry_point='learn_seq.envs.mujoco.insertion:MujocoInsertionEnv',
 )
 
-register(
-    id='RealInsertionEnv-v0',
-    entry_point='learn_seq.envs.real.insertion:RealInsertionEnv',
-)
+# register(
+#     id='RealInsertionEnv-v0',
+#     entry_point='learn_seq.envs.real.insertion:RealInsertionEnv',
+# )
 
 register(
     id='MujocoSlidingEnv-v0',
